3569d86c6e99e59552c3a6ce4f79b8ecdf190a84
Toufique18 <toufiqueeamam7@gmail.com>
2024-05-14 17:01:25 +0600

    footer done  and commit

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 const Root = () => {
@@ -7,7 +8,7 @@ const Root = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
-            
+            <Footer></Footer>
             
         </div>
     );
